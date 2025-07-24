5a9c6a2aaae5fb95593792aa310c872b16b5ddd4
Mooshiii <tbrooksdf@gmail.com>
2025-07-24 16:30:04 -0700

    Update KeithleyTestingPennathurLab.py

--- KeithleyTestingPennathurLab.py
+++ KeithleyTestingPennathurLab.py
@@ -9,29 +9,29 @@ VERSION = "Main: 1.2.0"
 #
 # Put email(s) you want data sent to here seperated by commas:
 # Ex.          = "email@gmail.com, email2@gmail.com, email3@gmail.com"
-emailAddresses = "tfrischknecht@ucsb.edu"
+emailAddresses = "email@gmail.com, cooleremail@gmail.com"
 #
 # Put the title of your test here:
 # Voltage is automatically added to the front! "Test1" -> "(5V) Test1"
 # Ex.    = "100mM NaCl Test 1"
-testName = "Air Test"
+testName = "Test Name Goes Here!"
 #
 # If you have any additional comments you would like to include, please include them here!
 # Otherwise, you may leave the quotation marks blank:
 # Ex.     = "We re-ran the test because the first chip was broken :("
-testNotes = "This is a test with both leads hovering in the air to see what happens when we run two Keithleys at once!"
+testNotes = "Notes for your test can go here! They are sent to your email."
 #
 # Put the voltage you'd like to source here (in V):
 # Ex.   = [5, 10, 20]
-voltage = [5]
+voltage = [0]
 #
 # Put the time you'd like the test to run here (in min):
 # Ex.    = [10, 10, 300]
-testTime = [0.5]
+testTime = [0]
 #
 # Put the GPIB Addresses of the Keithleys you would like to test. (You can use up to 30!)
 # Ex.           = [22, 24, 26]
-AddressesOfGPIB = [11, 30]
+AddressesOfGPIB = [0]
 #
 #
 #
@@ -46,11 +46,7 @@ AddressesOfGPIB = [11, 30]
 #
 ####################################################################################################
 # Please don't touch this part unless you know what you're doing, its important!!
-from dotenv import load_dotenv
-load_dotenv()
-try:
-    if not os.getenv("SMTP_USERNAME") or not os.getenv.("SMTP_PASSWORD"):
-        print("Something is wrong with your .env file. Is it present?")
+print("MAKE SURE YOU HAVE YOUR .env FILE CONFIGURED PROPERLY!")
 from helper.pnfltest import runTest
 if (len(voltage) != len(testTime)):
     print("Voltage list not same length as testTime list. Please ensure they both have the same number of items.")
